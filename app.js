495cca2a491140ca501269f4a30214d80ca5be38
nikiman <nikitarogacev8@gmail.com>
2026-02-17 11:44:50 +0600

    create logic of discount

---
+++ app.js
@@ -0,0 +1,16 @@
+var sum = Number(prompt("Введи сумму:"));
+
+if (isNaN(sum) || sum < 0) {
+  console.log("Введи корректное число");
+} else if (sum < 100) {
+  console.log(`Сумма: ${sum}$, скидки нет`);
+} else if (sum >= 100 && sum < 500) {
+  var discount = (sum * 5) / 100;
+  console.log(`Сумма: ${sum}$, скидка: ${discount.toFixed(2)}$`);
+} else if (sum >= 500 && sum <= 1000) {
+  var discount = (sum * 10) / 100;
+  console.log(`Сумма: ${sum}$, скидка: ${discount.toFixed(2)}$`);
+} else if (sum > 1000) {
+  var discount = (sum * 15) / 100;
+  console.log(`Сумма: ${sum}$, скидка: ${discount.toFixed(2)}$`);
+}
